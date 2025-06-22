874a8723170acc0616f4ad2c8424278d5717842a
Aaditya Jagdale <aadityajagdale.21@gmail.com>
2025-06-22 08:33:58 +0530

    gunicorn update

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 # FastAPI
 from fastapi import FastAPI
-import uvicorn
 from routes import routes
 
 app = FastAPI()
 
 app.include_router(routes.router)
-
-# Run the app
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
